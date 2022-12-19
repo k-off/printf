f4dd7c6972be27c19be77608b9eae30f14ab0972
pcovalio <pcovalio@student.42berlin.de>
2022-12-19 16:03:43 +0100

    dbl_decpart: remove unnecessary loop; add documenting comments to static functions

--- src/ft_dbltostr_base.c
+++ src/ft_dbltostr_base.c
@@ -6,15 +6,21 @@
 /*   By: pcovalio <pcovalio@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 14:09:41 by pcovalio          #+#    #+#             */
-/*   Updated: 2022/12/17 19:13:36 by pcovalio         ###   ########.fr       */
+/*   Updated: 2022/12/18 12:25:23 by pcovalio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include <stdio.h>
-#include <unistd.h>
 
-static char *infnan(long double n, t_bool upper)
+/**
+ * @brief Detect whether 80-bit extended precision number
+ * 		is set to one of the special states (-nan, nan, -inf, inf)
+ * 
+ * @param n long double, number to be converted
+ * @param upper t_bool, return an upper-case string if set to true
+ * @return char* result string, if number is in a special state or NULL
+ */
+static char	*infnan(long double n, t_bool upper)
 {
 	if (*((uint32_t *)(&((char *)(&n))[6])) == 0xFFFFC000)
 	{
@@ -40,9 +46,18 @@ static char *infnan(long double n, t_bool upper)
 			return (ft_strdup("-inf"));
 		return (ft_strdup("-INF"));
 	}
-	return NULL;
+	return (NULL);
 }
 
+/**
+ * @brief Convert decimal part of a float into a string with requitred base
+ * 		and case
+ * 
+ * @param n long double, number to be converted
+ * @param base int, required base (ie binary hex etc), bases 2-36 are supported
+ * @param upper t_bool, bases 10+ use uppercase letters if set to true
+ * @return char* 
+ */
 static char	*dbl_intpart(long double n, int base, t_bool upper)
 {
 	size_t	zeros;
@@ -59,6 +74,16 @@ static char	*dbl_intpart(long double n, int base, t_bool upper)
 	return (res);
 }
 
+/**
+ * @brief Convert decimal part of a float into a string with requitred base,
+ * 		case and precision
+ * 
+ * @param n long double, number to be converted
+ * @param base int, required base (ie binary hex etc), bases 2-36 are supported
+ * @param upper t_bool, bases 10+ use uppercase letters if set to true
+ * @param prec uint64_t, required precision
+ * @return char* 
+ */
 static char	*dbl_decpart(long double n, int base, t_bool upper, uint64_t prec)
 {
 	char		*r;
@@ -73,24 +98,27 @@ static char	*dbl_decpart(long double n, int base, t_bool upper, uint64_t prec)
 		++zeros_before;
 	}
 	r = NULL;
-	if (n != 0 && prec > 0)
-	{
-		lim = 19;
-		while (prec-- && lim--)
-			n *= 10;
-		r = string_joiner(2, r, ulltoa_base((uint64_t)n, base, upper, prec));
-		printf("\n%Lf: %s\n", n, r);
-	}
+	lim = 19;
+	while (prec && lim)
+		n *= (10 * (prec-- >= 1 && lim-- > 1));
+	r = string_joiner(2, r, ulltoa_base((uint64_t)n, base, upper, prec));
 	return (string_joiner(3, ft_stralloc('0', zeros_before), r, \
 							ft_stralloc('0', prec)));
 }
 
+/**
+ * @brief Round the least significant digit that has to be printed if needed.
+ * 		Only up to 19 decimal digits are handled.
+ * 
+ * @param n number to be printed
+ * @param prec reqired precision
+ */
 static void	ft_round(long double *n, uint64_t prec)
 {
 	long double	tmp;
-	long double cor;
+	long double	cor;
 
-	tmp = 10.0L;
+	tmp = 5.0L;
 	cor = 1.0L / (1ULL << 62);
 	tmp /= 10.0L;
 	while (prec--)
@@ -105,30 +133,20 @@ static void	ft_round(long double *n, uint64_t prec)
 
 char	*dbltostr_base(long double n, int base, t_bool upper, uint64_t prec)
 {
-	char *res;
+	char	*res;
 
 	if (base < 2 || base > 36)
 		return (NULL);
-	if (prec < 0)
-		return (NULL);
-	write(1, "0", 1);
 	res = infnan(n, upper);
-	write(1, "0", 1);
 	if (res != NULL)
 		return (res);
-	else
-		write(1, "0", 1);
 	if (n < 0 || (n == 0 && (*((uint8_t *)(&((char *)(&n))[9])) >> 7) == 1))
 	{
 		*((uint8_t *)(&((char *)(&n))[9])) ^= (1 << 7);
 		res = string_joiner(2, res, ft_strdup("-"));
 	}
-	write(1, "0", 1);
 	ft_round(&n, prec);
-	write(1, "1", 1);
 	res = string_joiner(2, res, dbl_intpart(n, base, upper));
-	printf("%s\n", res);
-write(1, "2", 1);
 	if (prec > 0 && res != NULL)
 		res = string_joiner(3, res, ft_strdup("."), \
 							dbl_decpart(n, base, upper, prec));
